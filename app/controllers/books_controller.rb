c27c4c6f531bc045e47d1a1d02f4cb8872d10f7e
EC2 Default User <ec2-user@ip-172-31-31-33.ec2.internal>
2022-06-08 12:39:09 +0000

    新規登録途中

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/'
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
