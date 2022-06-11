7066ff374851fa02d0401923ea7cbaae7677c9a1
EC2 Default User <ec2-user@ip-172-31-31-33.ec2.internal>
2022-06-11 05:34:25 +0000

    遷移編集完

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,9 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/'
+    redirect_to book_path(book.id)
   end
 
   def edit
@@ -21,12 +18,20 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+private
   def book_params
     params.require(:book).permit(:title, :body)
   end
